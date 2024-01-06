32e97580becf9866dbd107863d7f2a53a36c4676
Nick <noobping@users.noreply.github.com>
2024-01-06 17:35:59 +0100

    download and build

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-
-# Get wine version
-LATEST=$(curl -L "https://api.github.com/repos/mmtrt/WINE_AppImage/releases/latest" | jq -r .assets[0].browser_download_url)
-
+#
 # Download wine
-curl -L $LATEST -o AppDir/wine.AppImage
+LATEST_WINE=$(curl -L "https://api.github.com/repos/mmtrt/WINE_AppImage/releases/latest" | jq -r .assets[0].browser_download_url)
+curl -L $LATEST_WINE -o AppDir/wine.AppImage
 chmod +x AppDir/wine.AppImage
-
-# Download WinBox
+#
+# Download appimagetool
+LATEST_TOOL=$(curl -L "https://api.github.com/repos/AppImage/AppImageKit/releases/latest" | jq -r '.assets[] | select(.name | test("appimagetool-x86_64.AppImage$")) | .browser_download_url')
+curl -L $LATEST_TOOL -o appimagetool.AppImage
+chmod +x appimagetool.AppImage
+#
+# Download winbox
 curl -L https://mt.lv/winbox64 -o AppDir/winbox64.exe
+#
+# Build
+ARCH=x86_64 appimagetool.AppImage -v AppDir
