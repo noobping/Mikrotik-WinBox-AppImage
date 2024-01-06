40e76115cfec1e514b6f89eb7171357c5437a521
Nick <noobping@users.noreply.github.com>
2024-01-06 16:55:29 +0100

    use wine appimage

--- build.sh
+++ build.sh
@@ -1,44 +1,11 @@
 #!/bin/sh
 
-URL="https://github.com/GloriousEggroll/wine-ge-custom/releases/download/GE-Proton8-25/wine-lutris-GE-Proton8-25-x86_64.tar.xz"
-HASH="466f5d77d7b9821f1023859a5e467b9344afd25cb3018b04ac0e89c9d25fd8c0"
+# Get wine version
+LATEST=$(curl -L "https://api.github.com/repos/mmtrt/WINE_AppImage/releases/latest" | jq -r .assets[0].browser_download_url)
 
-echo "Downloading the file..."
-curl -L $URL -o wine-lutris.tar.xz
-curl -L https://mt.lv/winbox64 -o winbox64.exe
+# Download wine
+curl -L $LATEST -o AppDir/wine.AppImage
+chmod +x AppDir/wine.AppImage
 
-echo "Validating the hash..."
-DOWNLOADED_HASH=$(sha256sum wine-lutris.tar.xz | awk '{ print $1 }')
-
-if [ "$DOWNLOADED_HASH" != "$HASH" ]; then
-    echo "Hash mismatch. Exiting."
-    exit 1
-fi
-
-echo "Extracting the archive..."
-tar -xvf wine-lutris.tar.xz
-mv lutris* AppDir/usr
-mv winbox64.exe AppDir/winbox64.exe
-
-echo "Performing cleanup..."
-rm -rf *.tar.xz \
-       lutris* \
-       AppDir/usr/bin/msidb \
-       AppDir/usr/bin/wineconsole \
-       AppDir/usr/bin/msiexec \
-       AppDir/usr/bin/notepad \
-       AppDir/usr/bin/regedit \
-       AppDir/usr/bin/regsvr32 \
-       AppDir/usr/bin/wine-preloader \
-       AppDir/usr/bin/wine64-preloader \
-       AppDir/usr/bin/wineconsole \
-       AppDir/usr/bin/winedbg \
-       AppDir/usr/bin/winefile \
-       AppDir/usr/bin/winemine \
-       AppDir/usr/bin/winepath \
-       AppDir/usr/share/man \
-       AppDir/usr/share/applications/wine.desktop
-
-echo "Done"
-chmod +x AppDir/usr/bin/*
-chmod +x AppDir/AppRun
+# Download WinBox
+curl -L https://mt.lv/winbox64 -o AppDir/winbox64.exe
